f6bef9b99acc030e38f60aa58edae094e09df376
scapula07 <bartholomewonogwu@yahoo.com>
2022-09-17 02:12:10 -0700

    adding custom token to coinbabse

--- src/components/ConnectWallet/index.js
+++ src/components/ConnectWallet/index.js
@@ -21,6 +21,26 @@ export default function ConnectWallet() {
             .catch((e)=>{
               console.log(e)
             })
+
+
+            ethereum.request({
+              method: "wallet_watchAsset",
+              params: {
+                type: "ERC20",
+                options: {
+                  address: "0xE09Aa821C1480aE46364583563a500f1288f5E7D",
+                  symbol: "KLV",
+                  decimals: 18,
+                  image:
+                    "https://firebasestorage.googleapis.com/v0/b/kryptpay-f5b8a.appspot.com/o/mainlogo.png?alt=media&token=0ac99a8a-e9ba-4b80-ab69-16ff3af34f37",
+                },
+              },
+            })
+            .then(response => {
+              const result =
+              result ? onApproveWatchAsset() : onDenyWatchAsset()
+            })
+            .catch(err => onError(err.message));
       }catch(e){
         console.log(e)
       }
